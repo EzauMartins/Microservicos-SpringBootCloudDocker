2df73895db4aec09457c31879019e10b8655826e
Ezau SM <ezaumateus97@gmail.com>
2023-11-14 16:51:14 -0300

    configInicialServer

---
+++ Ez-Config-Server/src/test/java/com/example/ezconfigserver/EzConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ezconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
