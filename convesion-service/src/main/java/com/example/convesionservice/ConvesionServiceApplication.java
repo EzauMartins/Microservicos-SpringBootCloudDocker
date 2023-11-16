cc714aab788c53d5c96df91fcbf36b6a5aeb4e2d
Ezau SM <ezaumateus97@gmail.com>
2023-11-16 20:29:42 -0300

    novo servico

---
+++ convesion-service/src/main/java/com/example/convesionservice/ConvesionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.convesionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvesionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvesionServiceApplication.class, args);
+    }
+
+}
---
+++ convesion-service/src/test/java/com/example/convesionservice/ConvesionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convesionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvesionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
