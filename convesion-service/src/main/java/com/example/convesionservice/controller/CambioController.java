8e21dc4af1781f728ff8909509e1eec3abd464eb
Ezau SM <ezaumateus97@gmail.com>
2023-11-20 18:32:06 -0400

    conect database

--- convesion-service/src/main/java/com/example/convesionservice/controller/CambioController.java
+++ convesion-service/src/main/java/com/example/convesionservice/controller/CambioController.java
@@ -1,21 +1,42 @@
 package com.example.convesionservice.controller;
 
 import com.example.convesionservice.model.Cambio;
+import com.example.convesionservice.repository.CambioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private CambioRepository repository;
+
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal total,
                             @PathVariable("from") String from,
                             @PathVariable("to")String to){
-        return new Cambio(1L,from,to,BigDecimal.ONE,BigDecimal.ONE,"PORT 8000");
+
+        var cambio = repository.findByFromAndTo(from,to);
+        System.out.println(cambio);
+        if (cambio == null) throw new RuntimeException("Currency Unsuported");
+        var port = environment.getProperty("local.server.port");
+
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(total);
+        cambio.setEnvironment(port);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+        return cambio;
     }
+
 }
---
+++ convesion-service/src/main/java/com/example/convesionservice/repository/CambioRepository.java
@@ -0,0 +1,12 @@
+package com.example.convesionservice.repository;
+
+import com.example.convesionservice.model.Cambio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+    Cambio findByFromAndTo(String from, String to);
+
+}
